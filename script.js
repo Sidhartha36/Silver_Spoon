75d0ba95287b039288ae94b905e7ddc1c3bca5cd
Sidhartha36 <146981450+Sidhartha36@users.noreply.github.com>
2024-01-13 23:20:47 +0530

    files uploaded

---
+++ script.js
@@ -0,0 +1,18 @@
+let openShopping=document.querySelector('.shopping');
+let closeShopping = document.querySelector('.closeShopping');
+let list = document.querySelector('.list');
+let listCard = document.querySelector('.listCard');
+let body = document.querySelector('body');
+let total = document.querySelector('.total');
+let quantity = document.querySelector('.quantity');
+
+console.log(openShopping)
+openShopping.addEventListener('click',()=>{
+    body.classList.add('active');
+})
+closeShopping.addEventListener('click', ()=>{
+    body.classList.remove('active');
+})
+
+
+
